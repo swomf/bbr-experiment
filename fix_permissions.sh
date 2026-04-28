a174f11fb9b55057caeda06af642733153a5d52d
swomf <swomf@proton.me>
2026-04-28 19:06:01 -0400

    Fix top-level dir chowning in fix_permissions.sh

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 set -x
+sudo chmod 0755 results* out*
+sudo chown "$(whoami)":"$(whoami)" results* out*
 fd --no-ignore . --type f results* out* | xargs sudo chmod 0644
 fd --no-ignore . --type d results* out* | xargs sudo chmod 0755
 fd --no-ignore . results* out* | xargs sudo chown "$(whoami)":"$(whoami)"
